30fb6eafdeab78aa4afc8b25a06c49ead5f13c89
mashhood03344 <mashhoodhamid33@gmail.com>
2024-04-16 13:59:56 +0500

    updated server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-let userGoal = 'Learn Development Pipeline!!!!!!';
+let userGoal = 'Learn Development Pipeline!';
 
 app.use(
   bodyParser.urlencoded({
@@ -21,7 +21,7 @@ app.get('/', (req, res) => {
       </head>
       <body>
         <section>
-          <h2>My Course Goal Update!!!!!!!</h2>
+          <h2>My Course Goal Update!</h2>
           <h3>${userGoal}</h3>
         </section>
         <form action="/store-goal" method="POST">
@@ -41,6 +41,21 @@ app.post('/store-goal', (req, res) => {
   console.log(enteredGoal);
   userGoal = enteredGoal;
   res.redirect('/');
+});
+
+app.post('/github', (req, res) => {
+  // Call your deploy script here
+  const { exec } = require('child_process');
+  exec('sh /home/mash-hood/DRIVE D/DEVOPS LEARNING/Programmer Force Tasks/Task 1/deploy1.sh', (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.error(`stderr: ${stderr}`);
+  });
+
+  res.status(200).send('Webhook received');
 });
 
 app.listen(3000);
